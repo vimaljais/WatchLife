fbf3135356a21c3d4bf6dafdc7af4c666ddbae45
Vimal Jaiswal <jaiswalvimal1@gmail.com>
2020-09-06 10:44:56 +0530

    now wakes the server up when the client is called

--- src/Components/Logo/Logo.js
+++ src/Components/Logo/Logo.js
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import logoImage from "./../../img/logo.png";
+
 const Logo = () => {
+  const a = 0;
+
+  useEffect(() => {
+    const rep = fetch(`https://arcane-sierra-37425.herokuapp.com/`);
+    console.log(rep);
+  }, [a]);
   return (
     <img
       style={{ paddingTop: "30px" }}
